37dcfa98d48843adfe6eec1fa4cb34c45267d5c6
Crowdbotics <team@crowdbotics.com>
2021-10-22 15:32:24 +0000

    Initial commit.

---
+++ backend/watz_poppin_31643/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for watz_poppin_31643 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'watz_poppin_31643.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "watz_poppin_31643",
+  url: "https://watz_poppin_31643.botics.co",
+  api: "https://watz_poppin_31643.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
